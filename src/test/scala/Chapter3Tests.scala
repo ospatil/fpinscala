ca9ca372e84665220cdcb852bfab0e35eff9ec81
Omkar Patil <ospatil@gmail.com>
2021-02-03 19:41:47 -0500

    Reformat

--- src/test/scala/Chapter3Tests.scala
+++ src/test/scala/Chapter3Tests.scala
@@ -3,33 +3,39 @@ import org.junit.Assert._
 import List._
 
 class Chapter3Tests:
-  @Test def tailTest()=
-    assertEquals(List(2, 3), tail(List(1, 2, 3)))
+    @Test def tailTest()=
+        assertEquals(List(2, 3), tail(List(1, 2, 3)))
 
-  @Test(expected = classOf[RuntimeException]) def tailTestException(): Unit =
-    tail(Nil)
+    @Test(expected = classOf[RuntimeException]) def tailTestException(): Unit =
+        tail(Nil)
 
-  @Test def setHeadTest() =
-    assertEquals(List(0, 2), setHead(List(1, 2), 0))
+    @Test def setHeadTest() =
+        assertEquals(List(0, 2), setHead(List(1, 2), 0))
 
-  @Test def dropTest() =
-    assertEquals(List(2, 3), drop(List(1, 2, 3), 1))
-    assertEquals(List(1, 2, 3), drop(List(1, 2, 3), 0))
-    assertEquals(Nil, drop(List("a", "b"), 2))
-    assertEquals(Nil, drop(List(1, 2), 3))
-    assertEquals(Nil, drop(Nil, 1))
+    @Test def dropTest() =
+        assertEquals(List(2, 3), drop(List(1, 2, 3), 1))
+        assertEquals(List(1, 2, 3), drop(List(1, 2, 3), 0))
+        assertEquals(Nil, drop(List("a", "b"), 2))
+        assertEquals(Nil, drop(List(1, 2), 3))
+        assertEquals(Nil, drop(Nil, 1))
 
-  @Test def dropWhileTest() =
-    assertEquals(List(2, 3), dropWhile(List(1, 2, 3), x => x < 2))
-    assertEquals(List(1, 2, 3), dropWhile(List(1, 2, 3), x => x > 2))
-    assertEquals(Nil, dropWhile(List(1, 2, 3), x => x > 0))
-    assertEquals(Nil, dropWhile(Nil, (x: Int) => x > 0)) // Explicit type needed for x
-    assertEquals(List(3, 1, 2), dropWhile(List(3, 1, 2), x => x < 0))
+    @Test def dropWhileTest() =
+        assertEquals(List(2, 3), dropWhile(List(1, 2, 3), x => x < 2))
+        assertEquals(List(1, 2, 3), dropWhile(List(1, 2, 3), x => x > 2))
+        assertEquals(Nil, dropWhile(List(1, 2, 3), x => x > 0))
+        assertEquals(Nil, dropWhile(Nil, (x: Int) => x > 0)) // Explicit type needed for x
+        assertEquals(List(3, 1, 2), dropWhile(List(3, 1, 2), x => x < 0))
 
-  @Test def initTest() =
-    assertEquals(List(1, 2), init(List(1, 2, 3)))
-    assertEquals(List(1), init(List(1, 2)))
-    assertEquals(Nil, init(List(1)))
+    @Test def initTest() =
+        assertEquals(List(1, 2), init(List(1, 2, 3)))
+        assertEquals(List(1), init(List(1, 2)))
+        assertEquals(Nil, init(List(1)))
 
-  @Test(expected = classOf[RuntimeException]) def initTestException(): Unit =
-    init(Nil)
+    @Test(expected = classOf[RuntimeException]) def initTestException(): Unit =
+        init(Nil)
+
+    @Test def sum2Test() =
+        assertEquals(6, sum2(List(1, 2, 3)))
+
+    @Test def product2Test() =
+        assertEquals(6.0, product2(List(1.0, 2.0, 3.0)), 0.5) // 0.5 is delta required for float and double comparisons
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -3,5 +3,5 @@ import org.junit.Test
 import org.junit.Assert._
 
 class Test1:
-  @Test def t1() =
-    assertEquals("I was compiled by Scala 3. :)", msg)
+    @Test def t1() =
+        assertEquals("I was compiled by Scala 3. :)", msg)
